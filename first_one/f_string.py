13b55b0911e716f69d839626ecfeaffcba634abc
Abeer Azad <abeerazad19@gmail.com>
2019-10-10 10:59:19 +0600

    10th Oct commit

---
+++ first_one/f_string.py
@@ -0,0 +1,7 @@
+name = 'Abeer'
+age = 28
+
+my_greeting = 'My name is ' + name + ' and I am ' + str(age) + ' years old'
+python_greeting = 'I am {age} years old and my name is {name}'.format(name=name, age=age)
+#print(F"Hello, {name}. You are {age}.")
+print (my_greeting)
